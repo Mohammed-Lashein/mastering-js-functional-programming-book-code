a8b8ea9c3522fcba271074d086f11ffcd1ea022b
Mohammed-Lashein <mohammedlashein2700@gmail.com>
2025-09-01 15:31:42 +0300

    chore: format chapter2/main.js using prettier

--- chapter2/main.js
+++ chapter2/main.js
@@ -1,25 +1,25 @@
 export function once(fn) {
-  let done = false;
-  return (...args) => {
-    if(!done) {
-      done = true
-      fn(...args)
-    }
-  }
+	let done = false
+	return (...args) => {
+		if (!done) {
+			done = true
+			fn(...args)
+		}
+	}
 }
 export function onceAndAfter(f, g) {
-  let done = false
-  return (...args) => {
-    if(!done) {
-      done = true
-      // try logging the arguments to see them in the tests
-      console.log('these are args', args);
-      
-      f(...args)
-    }    
-    
-    if(done) {
-      g(...args)
-    }
-  }
+	let done = false
+	return (...args) => {
+		if (!done) {
+			done = true
+			// try logging the arguments to see them in the tests
+			console.log('these are args', args)
+
+			f(...args)
+		}
+
+		if (done) {
+			g(...args)
+		}
+	}
 }

466011edc8c152b52d618df11d8b7070eaa5236d
Mohammed-Lashein <mohammedlashein2700@gmail.com>
2025-09-01 15:39:23 +0300

    chore: format callingOnce.test.js file

--- chapter2/callingOnce.test.js
+++ chapter2/callingOnce.test.js
@@ -1,23 +1,23 @@
-import { once, onceAndAfter} from './main';
-import {jest} from '@jest/globals' // since we are using ESM, jest global object can't be injected
+import { once, onceAndAfter } from './main'
+import { jest } from '@jest/globals' // since we are using ESM, jest global object can't be injected
 
-  test("once() returns a function that is called only once", () => {
-    const func = jest.fn();
-    const onceFn = jest.fn(once(func));
+test('once() returns a function that is called only once', () => {
+	const func = jest.fn()
+	const onceFn = jest.fn(once(func))
 
-    onceFn()
-    onceFn()
-    onceFn()
-    expect(onceFn).toHaveBeenCalledTimes(3);
-    expect(func).toHaveBeenCalledTimes(1);
-  })
+	onceFn()
+	onceFn()
+	onceFn()
+	expect(onceFn).toHaveBeenCalledTimes(3)
+	expect(func).toHaveBeenCalledTimes(1)
+})
 
-test("onceAndAfter is working as expected", () => {
-  const fnOnce = jest.fn()
-  const fnAfter = jest.fn()
-  const onceAndAfterFn = jest.fn(onceAndAfter(fnOnce, fnAfter))
+test('onceAndAfter is working as expected', () => {
+	const fnOnce = jest.fn()
+	const fnAfter = jest.fn()
+	const onceAndAfterFn = jest.fn(onceAndAfter(fnOnce, fnAfter))
 
-  onceAndAfterFn()
+	onceAndAfterFn()
 	expect(fnOnce).toHaveBeenCalledTimes(1)
 	expect(fnAfter).toHaveBeenCalledTimes(0)
 

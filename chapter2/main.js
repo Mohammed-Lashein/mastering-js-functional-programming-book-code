8985a1e22f91775ef6b64adc420489844417f7b4
Mohammed-Lashein <mohammedlashein2700@gmail.com>
2025-09-01 15:32:49 +0300

    fix: fix the wrong implementation of onceAndAfter function as caught by the test

--- chapter2/main.js
+++ chapter2/main.js
@@ -16,10 +16,23 @@ export function onceAndAfter(f, g) {
 			console.log('these are args', args)
 
 			f(...args)
-		}
-
-		if (done) {
+		} else {
 			g(...args)
 		}
+		/* Wrong implementation after testing!
+      This implementation will result in this if block being executed in the first call to the 
+      function since the variable done will be true after the running of the 1st condition 
+      (which is not what we want).
+
+      That's why the writer used else instead of another if block. 
+
+      The proposed solution: 
+      [1] make the condition that calls function g at the top 
+      [2] Use else as the writer did
+    */
+
+		// if (done) {
+		// 	g(...args)
+		// }
 	}
 }
